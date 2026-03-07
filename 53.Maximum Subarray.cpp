4467e566ee8b6031440d76cfdca2e6dd1e85becc
nih8 <b24cs1045@iitj.ac.in>
2026-03-08 02:53:09 +0530

    Create 53.Maximum Subarray.cpp

---
+++ 53.Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxx=nums[0];
+        for(int i=1;i<nums.size();i++){
+            nums[i]=max(nums[i],nums[i]+nums[i-1]);
+            maxx = max(maxx,nums[i]);
+        }
+        return maxx;
+    }
+};
